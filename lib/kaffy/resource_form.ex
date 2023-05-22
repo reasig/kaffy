f05f2a4c30969a71baa1d92e487f6d32e987b13d
Adrian_Arcas <adrian.arcas@yahoo.com>
2023-05-22 17:13:33 +0300

    Make field optional

--- lib/kaffy/resource_form.ex
+++ lib/kaffy/resource_form.ex
@@ -472,7 +472,7 @@ defmodule Kaffy.ResourceForm do
     end
   end
 
-  def list_action_kaffy_input(form, field, type, title, options \\ []) do
+  def list_action_kaffy_input(form, field, title, type \\ :text, options \\ []) do
     label_tag = Atom.to_string(title)
     field_tag = generate_field_tag(field, type)
 
